3a99a9a3508db90d3a05deae048f652da4175cff
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2020-07-24 13:10:45 -0400

    Added day23

---
+++ day23.js
@@ -0,0 +1,41 @@
+// --- Day 23: Single Number III ---
+
+//
+
+// ----------
+
+// TO-DO: FIND A CONSTANT SPACE SOLUTION
+function solution_1 (nums) {
+
+}
+
+// thomas luo's one-liner - doesn't use constant space
+solution_2=(n,s={})=>n.map(e=>s[e]=s[e]?2:1)&&n.filter(e=>s[e]-2)
+
+// my improvement on thomas' one-liner (`f[n]` gets assigned to its reverse boolean value, and we filter by truthy values of `f[n]`)
+solution_3=(N,f={})=>N.map(n=>f[n]=!f[n])&&N.filter(n=>f[n])
+
+const singleNumber = solution_3;
+
+// const specialTest = (...args) => {
+// };
+
+// TEST CASES
+
+const test = require('./_test');
+const testNum = [1];
+let input, expected;
+const func = singleNumber;
+const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
+const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
+const lowestTest = 0 || 0;
+const highestTest = 0 || Infinity;
+
+// Test case 1
+input = {
+  nums: [1, 2, 1, 3, 2, 5],
+};
+expected = [3, 5];
+test(sortedFunc, input, expected.sort(), testNum, lowestTest, highestTest);
+
+// INITIALLY FAILED THESE TEST CASES:
